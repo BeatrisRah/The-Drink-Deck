272fd9882c2c96f42493c2d3957d11a7e506efc6
Beatris <beatrisraj7@gmail.com>
2024-12-27 22:41:33 +0200

    Home page-hamburger menu
    Hamburger menu added and functional

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-console.log('works');
+import { render } from "lit-html";
+import page from "page";
+import { homeView } from "./pages/home.js";
+import { updateNav } from "./nav.js";
+
+const root = document.getElementById('root');
+page(updateCTX)
+page(updateNav)
+page('/',homeView)
+page()
+
+function updateCTX(ctx, next){
+    ctx.render = rerender;
+    ctx.baseRender = baserender;
+    next()
+}
+function rerender(templ){
+    render(templ, root)
+}
+function baserender(templ,container){
+    render(templ,container)
+}
