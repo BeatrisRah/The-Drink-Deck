668cae9a9d42d106bb340f91bdcfd8106ea68320
Beatris <beatrisraj7@gmail.com>
2025-01-26 18:37:01 +0200

    Fixed error with all category page

--- src/pages/catalog.js
+++ src/pages/catalog.js
@@ -68,6 +68,8 @@ export async function catalog(ctx){
 
     if(drink_category !== 'all'){
         drinks = Object.values(drinks).filter(el => el.category == drink_category)
+    } else{
+        drinks = Object.values(drinks)
     }
     
     
